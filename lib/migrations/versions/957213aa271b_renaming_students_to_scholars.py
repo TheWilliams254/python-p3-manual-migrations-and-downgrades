007e54a53294cf7e50d537265ce2270e3b9e5a41
william <williamwambugu663@gmail.com>
2025-05-25 12:47:26 +0300

    Alembic and migrations

---
+++ lib/migrations/versions/957213aa271b_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 957213aa271b
+Revises: 791279dd0760
+Create Date: 2025-05-25 12:31:46.280965
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '957213aa271b'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
